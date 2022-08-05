822262dbf963416499a018cae52cc92a1099a4bb
craftablescience <brendanelewis@gmail.com>
2022-08-04 23:01:20 -0400

    fix: make autoresponder check lowercase message

--- src/utils/autorespond.ts
+++ src/utils/autorespond.ts
@@ -5,13 +5,15 @@ import * as config from '../config.json';
 export async function messageNeedsResponse(msg: Message<boolean>): Promise<string | undefined> {
 	if (msg.author.bot || msg.cleanContent.length <= 0) return undefined;
 
+	const messageContent = msg.cleanContent.toLowerCase();
+
 	for (const message of config.messages) {
 		if (message.keywords !== undefined && message.keywords_needed !== undefined && message.keywords_needed > 0) {
 			let keywordsFound = 0;
 			for (const keywordCombo of message.keywords) {
 				keywordLoop:
 				for (const keyword of keywordCombo) {
-					if (msg.content.includes(keyword)) {
+					if (messageContent.includes(keyword)) {
 						keywordsFound++;
 						break keywordLoop;
 					}
