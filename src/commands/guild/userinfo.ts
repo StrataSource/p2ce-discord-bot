c580d63e2569bc14723b796e62d08592ee65163d
craftablescience <brendanelewis@gmail.com>
2022-09-04 14:05:12 -0400

    fix: format userinfo command

--- src/commands/guild/userinfo.ts
+++ src/commands/guild/userinfo.ts
@@ -1,38 +1,34 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction, EmbedBuilder } from "discord.js";
-import { Command } from "../../types/command";
-import { LogLevelColor } from "../../utils/log";
-import { PermissionLevel } from "../../utils/permissions";
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import { Command } from '../../types/command';
+import { LogLevelColor } from '../../utils/log';
+import { PermissionLevel } from '../../utils/permissions';
 
 const UserInfo: Command = {
-    permissionLevel: PermissionLevel.MEMBER,
+	permissionLevel: PermissionLevel.MEMBER,
 
-    data: new SlashCommandBuilder()
-        .setName("userinfo")
-        .setDescription("Displays the user information of the message author."),
+	data: new SlashCommandBuilder()
+		.setName('userinfo')
+		.setDescription('Displays the user information of the message author.'),
 
-    async execute(interaction: CommandInteraction) {
-        const user = interaction.options.getUser("user") ?? interaction.user;
-        const name = `${user.username}#${user.discriminator}`;
-        const avatar = user.avatarURL({ size: 1024 }) ?? "";
+	async execute(interaction: CommandInteraction) {
+		const user = interaction.options.getUser('user') ?? interaction.user;
+		const name = `${user.username}#${user.discriminator}`;
+		const avatar = user.avatarURL({ size: 1024 });
 
-        // The EMBED
-        const embed = new EmbedBuilder()
-            .setColor(LogLevelColor.INFO)
-            .setTitle(user.username)
-            .setAuthor({
-                name: name,
-                iconURL: avatar,
-            })
-            .setDescription(`Your ID: ${user.id}`)
-            .setThumbnail(avatar)
-            .addFields({
-                name: "Joined Discord",
-                value: `<t:${(user.createdAt.getTime() / 1000).toFixed(0)}:D>`,
-            })
-            .setTimestamp();
+		const embed = new EmbedBuilder()
+			.setColor(LogLevelColor.INFO)
+			.setTitle(user.username)
+			.setAuthor({ name: name, iconURL: avatar ?? undefined })
+			.setDescription(`Your ID: ${user.id}`)
+			.setThumbnail(avatar)
+			.addFields({
+				name: 'Joined Discord',
+				value: `<t:${(user.createdAt.getTime() / 1000).toFixed(0)}:D>`,
+			})
+			.setTimestamp();
 
-        return interaction.reply({ embeds: [embed] });
-    },
+		return interaction.reply({ embeds: [embed] });
+	},
 };
 export default UserInfo;
