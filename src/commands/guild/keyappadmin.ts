05f3b53639b69c7f8a2d11c76b541ad4e21a3923
craftablescience <me@craftablescience.info>
2022-09-24 02:22:20 -0400

    feat: allow reading key app contents from sheet

---
+++ src/commands/guild/keyappadmin.ts
@@ -0,0 +1,49 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+import { Command } from '../../types/command';
+import { PermissionLevel } from '../../utils/permissions';
+import { isSheetLoaded } from '../../utils/sheet';
+import { checkUserKeyStatus, readUserApplication } from '../../utils/sheet-check';
+
+const KeyAppAdmin: Command = {
+	permissionLevel: PermissionLevel.TEAM_MEMBER,
+
+	data: new SlashCommandBuilder()
+		.setName('keyappadmin')
+		.setDescription('Various key application utilities for P2CE team members.')
+		.addSubcommand(subcommand => subcommand
+			.setName('check')
+			.setDescription('Check the status of your key application.')
+			.addUserOption(option => option
+				.setName('user')
+				.setDescription('The user to check the application of')
+				.setRequired(true)))
+		.addSubcommand(subcommand => subcommand
+			.setName('read')
+			.setDescription('Reads your latest key application.')
+			.addUserOption(option => option
+				.setName('user')
+				.setDescription('The user to read the application of')
+				.setRequired(true))),
+
+	async execute(interaction: CommandInteraction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		if (!isSheetLoaded()) {
+			return interaction.reply({ content: 'Sheet has not finished loading, please try again later.', ephemeral: true });
+		}
+
+		// Don't run command if already present in list
+		// If not present, add user to list
+		switch (interaction.options.getSubcommand()) {
+		case 'check': {
+			return checkUserKeyStatus(interaction, interaction.options.getUser('user', true));
+		}
+
+		case 'read': {
+			return readUserApplication(interaction, interaction.options.getUser('user', true));
+		}
+		}
+	}
+};
+export default KeyAppAdmin;
