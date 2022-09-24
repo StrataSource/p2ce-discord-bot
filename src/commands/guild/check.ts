0cdc7d084381c5be6fd07e2c072d642f0bd035da
AWildErin <iansharp1@live.co.uk>
2022-09-24 03:31:59 +0100

    fix: Requested Changes

--- src/commands/guild/check.ts
+++ src/commands/guild/check.ts
@@ -1,8 +1,13 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { Collection, CommandInteraction } from 'discord.js';
 import { Command } from '../../types/command';
 import { PermissionLevel } from '../../utils/permissions';
-import { CheckUserKeyStatus } from '../../utils/sheet-check';
+import { checkUserKeyStatus } from '../../utils/sheet-check';
+
+import * as config from '../../config.json';
+
+// I don't care if this gets flushed when the bot restarts
+const USER_DB = new Collection<string, number>();
 
 const Check: Command = {
 	permissionLevel: PermissionLevel.MEMBER,
@@ -12,7 +17,15 @@ const Check: Command = {
 		.setDescription('Check the status of your key application.'),
 
 	async execute(interaction: CommandInteraction) {
-		CheckUserKeyStatus(interaction);
+		// Don't run command if already present in list
+		// If not present, add user to list
+		if (USER_DB.has(interaction.user.id) && (USER_DB.get(interaction.user.id) ?? 0) > Date.now()) {
+			return interaction.followUp(`You have already checked the status of your application today. Please check again <t:${((USER_DB.get(interaction.user.id) ?? 0) / 1000).toFixed(0)}:R>.`);
+		} else {
+			USER_DB.set(interaction.user.id, Date.now() + (1000 * 60 * 60 * config.options.misc.key_check_hours_to_wait));
+		}
+
+		return checkUserKeyStatus(interaction, interaction.user);
 	}
 };
 export default Check;
