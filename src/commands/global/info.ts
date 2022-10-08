342ad01eb81c0052f4ec5f84061635e22ed59275
craftablescience <me@craftablescience.info>
2022-10-08 15:04:50 -0400

    feat: add bot invite link to info command

--- src/commands/global/info.ts
+++ src/commands/global/info.ts
@@ -1,10 +1,12 @@
-import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, EmbedBuilder, Emoji, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../types/interaction';
 import { LogLevelColor } from '../../utils/log';
 import { PermissionLevel } from '../../utils/permissions';
 
+import * as config from '../../config.json';
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJSON: { version: number, dependencies: unknown } = require('../../../package.json');
+const packageJSON: { version: number, dependencies: { 'discord.js': string } } = require('../../../package.json');
 
 const Info: Command = {
 	permissionLevel: PermissionLevel.EVERYONE,
@@ -27,14 +29,21 @@ const Info: Command = {
 			.setColor(LogLevelColor.INFO)
 			.addFields(
 				{ name: 'Version', value: `${packageJSON.version}`, inline: true },
-				{ name: 'Discord.JS', value: (packageJSON.dependencies as { 'discord.js': string })['discord.js'].substring(1), inline: true },
+				{ name: 'Discord.JS', value: packageJSON.dependencies['discord.js'].substring(1), inline: true },
 				{ name: 'Node.JS', value: process.versions.node, inline: true },
 				{ name: 'Memory Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
 				{ name: 'Uptime', value: `${(interaction.client.uptime / 1000 / 60 / 60).toFixed(3)} hours`, inline: true },
 				{ name: 'Servers', value: `${(await interaction.client.guilds.fetch()).size}`, inline: true })
 			.setTimestamp();
 
-		return interaction.reply({ embeds: [embed] });
+		const inviteButton = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(
+				new ButtonBuilder()
+					.setLabel('Add to Server')
+					.setStyle(ButtonStyle.Link)
+					.setURL(`https://discord.com/api/oauth2/authorize?client_id=${config.client_id}&permissions=8&scope=bot%20applications.commands`));
+
+		return interaction.reply({ embeds: [embed], components: [inviteButton] });
 	}
 };
 export default Info;
