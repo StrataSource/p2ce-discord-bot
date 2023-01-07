6cb5ff2db3a31b25b2b0ffc429bfdcbd34b61545
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-01-07 23:59:33 +0100

    add simple inter process communication

---
+++ src/utils/ipc.ts
@@ -0,0 +1,30 @@
+import {Server, connect} from 'net';
+import {ipc_port} from '../config.json';
+import {Collection} from 'discord.js';
+
+const requestHandlers: Collection<string, () => void> = new Collection();
+
+export function on( event: string, handler: () => void) {
+	console.log( `A listener for the event ${event} was registered` );
+	requestHandlers.set( event, handler );
+}
+export function send( message: string ) {
+	console.log( `Sending command '${message}' to running instance..` );
+	const connection = connect(ipc_port);
+	connection.on('ready', () => {
+		connection.on('drain', () => connection.destroy() );
+		if ( connection.write(message) )
+			connection.destroy();
+	});
+}
+export async function listen() {
+	console.log( `Listening for commands at ${ipc_port}!` );
+	const server = new Server();
+	server.on('connection', connection => {
+		connection.on('data', data => {
+			console.log( `Received command '${data.toString()}'!` );
+			requestHandlers.get( data.toString() )?.();
+		});
+	});
+	await server.listen( ipc_port );
+}
