b76842892e302b276cad8f18d35d8d2f090edac1
craftablescience <me@craftablescience.info>
2023-02-07 16:44:28 -0500

    fix: portal command links messages together

--- src/commands/guild/portal.ts
+++ src/commands/guild/portal.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, SlashCommandBuilder, TextChannel, ThreadChannel } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, CommandInteraction, SlashCommandBuilder, TextChannel, ThreadChannel } from 'discord.js';
 import { Command } from '../../types/interaction';
 import { PermissionLevel } from '../../utils/permissions';
 
@@ -11,6 +11,7 @@ const Portal: Command = {
 		.addChannelOption(option => option
 			.setName('channel')
 			.setDescription('The name of the channel to move to')
+			.addChannelTypes(ChannelType.GuildText | ChannelType.GuildPublicThread | ChannelType.PublicThread)
 			.setRequired(true)),
 
 	async execute(interaction: CommandInteraction) {
@@ -23,27 +24,38 @@ const Portal: Command = {
 		if (!interaction.channel || !channel) {
 			return interaction.reply({ content: `Channel ${channel} does not appear to exist.` });
 		}
-
-		if (channel instanceof TextChannel || channel instanceof ThreadChannel) {
-			const buttonTo = new ButtonBuilder()
-				.setStyle(ButtonStyle.Link)
-				.setEmoji('🔗')
-				.setLabel('from #' + interaction.channel.name)
-				.setURL(interaction.channel.url);
-			const buttonBarTo = new ActionRowBuilder<ButtonBuilder>()
-				.addComponents(buttonTo);
-			await channel.send({ components: [buttonBarTo] });
+		if (!(channel instanceof TextChannel || channel instanceof ThreadChannel)) {
+			return interaction.reply({ content: `Channel ${channel} is not a text channel or public thread.` });
 		}
+
+		const buttonToInitial = new ButtonBuilder()
+			.setStyle(ButtonStyle.Link)
+			.setEmoji('🔗')
+			.setLabel('from #' + interaction.channel.name)
+			.setURL(interaction.channel.url);
+		const buttonBarToInitial = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(buttonToInitial);
+		const messageTo = await channel.send({ components: [buttonBarToInitial] });
 
 		const buttonFrom = new ButtonBuilder()
 			.setStyle(ButtonStyle.Link)
 			.setEmoji('🔗')
 			.setLabel('to #' + channel.name)
-			.setURL(channel.url);
+			.setURL(messageTo.url);
 		const buttonBarFrom = new ActionRowBuilder<ButtonBuilder>()
 			.addComponents(buttonFrom);
+		const messageFrom = await interaction.channel.send({ components: [buttonBarFrom] });
 
-		return interaction.reply({ components: [buttonBarFrom] });
+		const buttonTo = new ButtonBuilder()
+			.setStyle(ButtonStyle.Link)
+			.setEmoji('🔗')
+			.setLabel('from #' + interaction.channel.name)
+			.setURL(messageFrom.url);
+		const buttonBarTo = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(buttonTo);
+		await messageTo.edit({ components: [buttonBarTo] });
+
+		return interaction.reply({ content: `Created portal to channel ${channel}!`, ephemeral: true });
 	}
 };
 export default Portal;
