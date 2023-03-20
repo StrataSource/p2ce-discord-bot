13aa6f8fa6addc219823ebd59c97377bdee1543d
craftablescience <me@craftablescience.info>
2023-03-19 23:32:52 -0400

    feat: add /fun image overlay command

--- src/commands/global/fun.ts
+++ src/commands/global/fun.ts
@@ -124,7 +124,23 @@ const Fun: Command = {
 					.setName('y')
 					.setDescription('The Y position of the tip of the speech bubble, from 0-1 (with (0,0) as the top left of the image)')
 					.setMinValue(0)
-					.setMaxValue(1)))),
+					.setMaxValue(1)))
+			.addSubcommand(subcommand => subcommand
+				.setName('overlay')
+				.setDescription('Overlay a given image over the base image.')
+				.addAttachmentOption(option => option
+					.setName('base')
+					.setDescription('The base image')
+					.setRequired(true))
+				.addAttachmentOption(option => option
+					.setName('overlay')
+					.setDescription('The image to overlay on top of the base image')
+					.setRequired(true))
+				.addNumberOption(option => option
+					.setName('opacity')
+					.setDescription('The opacity of the overlay image, from 0-100 (default 30)')
+					.setMinValue(0)
+					.setMaxValue(100)))),
 
 	async execute(interaction: CommandInteraction) {
 		if (!interaction.isChatInputCommand() || !interaction.channel) return;
@@ -286,6 +302,47 @@ const Fun: Command = {
 						.setName(`${imageName}_speech_bubble.gif`);
 				}
 				return interaction.editReply({ files: [attachment] });
+			}
+
+			case 'overlay': {
+				const base = interaction.options.getAttachment('base', true);
+				const overlay = interaction.options.getAttachment('overlay', true);
+
+				if (!base.contentType || !base.contentType.startsWith('image')) {
+					return interaction.reply({ content: 'File attached for `base` does not appear to be an image!', ephemeral: true });
+				}
+				if (!overlay.contentType || !overlay.contentType.startsWith('image')) {
+					return interaction.reply({ content: 'File attached for `overlay` does not appear to be an image!', ephemeral: true });
+				}
+
+				const baseResponse = await fetch(base.url);
+				if (!baseResponse.body) {
+					return interaction.reply({ content: 'Unable to download attachment.', ephemeral: true });
+				}
+				const overlayResponse = await fetch(overlay.url);
+				if (!overlayResponse.body) {
+					return interaction.reply({ content: 'Unable to download attachment.', ephemeral: true });
+				}
+
+				const baseBuf = Buffer.from(await baseResponse.arrayBuffer());
+				const drawableImageBase = await loadImage(baseBuf);
+				const overlayBuf = Buffer.from(await overlayResponse.arrayBuffer());
+				const drawableImageOverlay = await loadImage(overlayBuf);
+
+				const canvas = createCanvas(drawableImageBase.width, drawableImageBase.height);
+				const ctx = canvas.getContext('2d');
+
+				ctx.drawImage(drawableImageBase, 0, 0);
+				ctx.globalAlpha = (interaction.options.getNumber('opacity') ?? 30) / 100;
+				ctx.drawImage(drawableImageOverlay, 0, 0, canvas.width, canvas.height);
+
+				let baseName = 'image';
+				if (base.name) {
+					baseName = base.name.slice(0, base.name.lastIndexOf('.'));
+				}
+				const attachment = new AttachmentBuilder(canvas.createPNGStream())
+					.setName(`${baseName}_plus_overlay.png`);
+				return interaction.reply({ files: [attachment] });
 			}
 			}
 			break;
