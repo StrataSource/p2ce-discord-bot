90a2ae00a13694f11323f639ca414c3fe91f416d
craftablescience <me@craftablescience.info>
2023-03-25 19:38:39 -0400

    fix: use a service account to access the spreadsheets api

--- src/types/google-spreadsheet.d.ts
+++ src/types/google-spreadsheet.d.ts
@@ -13,6 +13,18 @@ declare module 'google-spreadsheet' {
 	export class GoogleSpreadsheet {
 		constructor(sheet_key: string);
 		useApiKey(key: string): void;
+		useServiceAccountAuth(creds: {
+			type: string,
+			project_id: string,
+			private_key_id: string,
+			private_key: string,
+			client_email: string,
+			client_id: string,
+			auth_uri: string,
+			token_uri: string,
+			auth_provider_x509_cert_url: string,
+			client_x509_cert_url: string,
+		}): Promise<void>;
 		loadInfo(): Promise<void>;
 		sheetsByIndex: Array<GoogleSpreadsheetWorksheet>;
 	}	
--- src/utils/sheet.ts
+++ src/utils/sheet.ts
@@ -5,10 +5,15 @@ import * as config from '../config.json';
 let sheets: GoogleSpreadsheet;
 export let sheet: GoogleSpreadsheetWorksheet;
 
-if (config.keyapp.auth.api_key.length > 0) {
+if (config.keyapp.auth.sheet_id.length > 0) {
 	sheets = new GoogleSpreadsheet(config.keyapp.auth.sheet_id);
-	sheets.useApiKey(config.keyapp.auth.api_key);
-	sheets.loadInfo().then(() => sheet = sheets.sheetsByIndex[0]);
+	sheets.useServiceAccountAuth(
+		config.keyapp.auth.credentials
+	).then(
+		() => sheets.loadInfo()
+	).then(
+		() => sheet = sheets.sheetsByIndex[0]
+	);
 }
 
 export function isSheetLoaded() {
