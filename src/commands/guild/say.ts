7d5163aa16623b798b5708a81e67759cd3286b0b
craftablescience <me@craftablescience.info>
2023-08-09 00:35:55 -0400

    feat: allow say command to send messages to other channels

--- src/commands/guild/say.ts
+++ src/commands/guild/say.ts
@@ -1,25 +1,37 @@
-import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
-import { Command } from '../../types/interaction';
-import { PermissionLevel } from '../../utils/permissions';
+import {ChannelType, CommandInteraction, SlashCommandBuilder} from 'discord.js';
+import {Command} from '../../types/interaction';
+import {PermissionLevel} from '../../utils/permissions';
 
 const Say: Command = {
 	permissionLevel: PermissionLevel.TEAM_MEMBER,
 
 	data: new SlashCommandBuilder()
 		.setName('say')
-		.setDescription('Repeat the given text in this channel.')
+		.setDescription('Sends a message in the given channel.')
 		.addStringOption(option => option
 			.setName('message')
-			.setDescription('The message to repeat')
-			.setRequired(true)),
+			.setDescription('The message to send')
+			.setRequired(true))
+		.addChannelOption(option => option
+			.setName('channel')
+			.setDescription('The channel to send the message to')
+			.addChannelTypes(
+				ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread,
+			)),
 
 	async execute(interaction: CommandInteraction) {
 		if (!interaction.isChatInputCommand()) return;
 
 		const message = interaction.options.getString('message', true);
-		interaction.channel?.send(message);
-
-		return interaction.reply({ content: `Echoed "${message}" to ${interaction.channel?.toString()}`, ephemeral: true });
+		const channel = interaction.options.getChannel('channel', false, [
+			ChannelType.GuildText, ChannelType.PublicThread, ChannelType.PrivateThread,
+		]) ?? interaction.channel;
+		try {
+			await channel?.send(message);
+		} catch (ignored) {
+			return interaction.reply({ content: `Error encountered when sending message "${message}" to ${channel}. Check channel permissions are correct!`, ephemeral: true });
+		}
+		return interaction.reply({ content: `Echoed "${message}" to ${channel}.`, ephemeral: true });
 	}
 };
 export default Say;
