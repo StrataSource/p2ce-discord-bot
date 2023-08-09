e457a76a8e75a5aef332e2fb663ad705a93962e0
craftablescience <me@craftablescience.info>
2023-08-08 23:32:34 -0400

    feat: add unbanid command

---
+++ src/commands/guild/unbanid.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { Command } from '../../types/interaction';
+import { PermissionLevel } from '../../utils/permissions';
+
+const UnBanID: Command = {
+	permissionLevel: PermissionLevel.MODERATOR,
+
+	data: new SlashCommandBuilder()
+		.setName('unbanid')
+		.setDescription('Unbans the user with the given ID.')
+		.addStringOption(option => option
+			.setName('id')
+			.setDescription('The user ID to unban')
+			.setRequired(true)),
+
+	async execute(interaction: CommandInteraction) {
+		if (!interaction.isChatInputCommand()) return;
+		if (!interaction.inGuild() || !interaction.guild) {
+			return interaction.reply({ content: 'This command must be ran in a guild.', ephemeral: true });
+		}
+
+		const id = interaction.options.getString('id', true);
+
+		const user = await interaction.client.users.fetch(id);
+		await interaction.guild.bans.remove(user);
+
+		return interaction.reply({ content: `Unbanned user with ID ${id}`, ephemeral: true });
+	}
+};
+export default UnBanID;
